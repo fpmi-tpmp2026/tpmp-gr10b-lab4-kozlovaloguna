f7f6129be4e3c2b65158ec930b32c6cad3e48a8c
Ffawka <miss94961@gmail.com>
2026-03-29 18:25:37 +0300

    Create schema.sql

---
+++ Task5/schema.sql
@@ -0,0 +1,9 @@
+-- Тест 1.1: Создание и проверка структуры
+SELECT name FROM sqlite_master WHERE type='table' AND name='FLOWERS';
+
+-- Тест 1.2: Проверка наличия колонок
+PRAGMA table_info(FLOWERS);
+
+-- Тест 1.3: Проверка PRIMARY KEY и AUTOINCREMENT
+INSERT INTO FLOWERS (flower, sort, price) VALUES ('Роза', 'Красная', 150.00);
+SELECT last_insert_rowid(); -- должно вернуть 1
